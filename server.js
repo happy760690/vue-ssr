dcfc31e12fd4a9ef09961c38da4b858ae459fb13
glitrma <237988427@qq.com>
2021-12-02 11:12:54 +0800

    - Create a template renderer

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const Static = require('koa-static')
 const Vue = require('vue')
+const fs = require('fs')
+const path = require('path')
 const VueServerRender = require('vue-server-renderer')
 
 const app = new Koa({})
 const router = new Router()
-const render = VueServerRender.createRenderer()
+
+// Create a renderer
+const template = fs.readFileSync(path.join(__dirname, './template.html'), 'utf8')
+const render = VueServerRender.createRenderer({
+    template: template,
+})
 
 const vm = new Vue({
     data(){
